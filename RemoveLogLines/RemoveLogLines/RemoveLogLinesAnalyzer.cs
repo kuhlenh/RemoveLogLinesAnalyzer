c1c7f85294b6020b36428a33661a0f744b4590c1
Kasey Uhlenhuth <kaseyu@microsoft.com>
2018-05-06 17:34:10 -0700

    cleanup

--- RemoveLogLines/RemoveLogLines/RemoveLogLinesAnalyzer.cs
+++ RemoveLogLines/RemoveLogLines/RemoveLogLinesAnalyzer.cs
@@ -1,52 +1,37 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-namespace RemoveLogLines
-{
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class RemoveLogLinesAnalyzer : DiagnosticAnalyzer
-    {
-        public const string DiagnosticId = "RemoveLogLines";
+namespace RemoveLogLines {
+	[DiagnosticAnalyzer(LanguageNames.CSharp)]
+	public class RemoveLogLinesAnalyzer : DiagnosticAnalyzer {
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Clean up";
+		public const string DiagnosticId = "RemoveLogLines";
+		private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+		private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+		private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+		private const string Category = "Clean up";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
+		private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: Description);
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+		public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
-        public override void Initialize(AnalysisContext context)
-        {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.InvocationExpression);
-        }
+		public override void Initialize(AnalysisContext context) {
+			context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.InvocationExpression);
+		}
 
-        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
-        {
-            var invocation = (InvocationExpressionSyntax)context.Node;
-            var symbolInfo = context.SemanticModel.GetSymbolInfo(invocation.Expression);
-            if (symbolInfo.Symbol is IMethodSymbol methodSymbol)
-            {
-                //just cut corners here and dont do all the checks
-                if(methodSymbol.Name == "LogLine")
-                {
-                    var diagnostic = Diagnostic.Create(Rule, invocation.GetLocation());
-                    context.ReportDiagnostic(diagnostic);
-                }
+		private void AnalyzeNode(SyntaxNodeAnalysisContext context) {
+			var invocation = (InvocationExpressionSyntax)context.Node;
+			var symbolInfo = context.SemanticModel.GetSymbolInfo(invocation.Expression);
+			if (symbolInfo.Symbol is IMethodSymbol methodSymbol) {
+				if (methodSymbol.Name == "LogLine") {
+					var diagnostic = Diagnostic.Create(Rule, invocation.GetLocation());
+					context.ReportDiagnostic(diagnostic);
+				}
 
-            }
-        }
-    }
+			}
+		}
+	}
 }
--- RemoveLogLines/RemoveLogLines/RemoveLogLinesCodeFixProvider.cs
+++ RemoveLogLines/RemoveLogLines/RemoveLogLinesCodeFixProvider.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
@@ -8,57 +6,45 @@ using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CodeActions;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Text;
 
-namespace RemoveLogLines
-{
-    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(RemoveLogLinesCodeFixProvider)), Shared]
-    public class RemoveLogLinesCodeFixProvider : CodeFixProvider
-    {
-        private const string title = "Remove log";
+namespace RemoveLogLines {
+	[ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(RemoveLogLinesCodeFixProvider)), Shared]
+	public class RemoveLogLinesCodeFixProvider : CodeFixProvider {
+		
+		private const string title = "Remove log";
 
-        public sealed override ImmutableArray<string> FixableDiagnosticIds
-        {
-            get { return ImmutableArray.Create(RemoveLogLinesAnalyzer.DiagnosticId); }
-        }
+		public sealed override ImmutableArray<string> FixableDiagnosticIds {
+			get { return ImmutableArray.Create(RemoveLogLinesAnalyzer.DiagnosticId); }
+		}
 
-        public sealed override FixAllProvider GetFixAllProvider()
-        {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
-            return WellKnownFixAllProviders.BatchFixer;
-        }
+		public sealed override FixAllProvider GetFixAllProvider() {
+			return WellKnownFixAllProviders.BatchFixer;
+		}
 
-        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
-        {
-            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+		public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context) {
+			var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
-            var diagnostic = context.Diagnostics.First();
-            var diagnosticSpan = diagnostic.Location.SourceSpan;
+			var diagnostic = context.Diagnostics.First();
+			var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the type declaration identified by the diagnostic.
-            //var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
-            var expression = root.FindNode(diagnosticSpan);
-            var expressionStatement = expression.FirstAncestorOrSelf<ExpressionStatementSyntax>();
+			var expression = root.FindNode(diagnosticSpan);
+			var expressionStatement = expression.FirstAncestorOrSelf<ExpressionStatementSyntax>();
 
-            // Register a code action that will invoke the fix.
-            context.RegisterCodeFix(
-                CodeAction.Create(
-                    title: title,
-                    createChangedDocument: c => RemoveLogsAsync(context.Document, expressionStatement, c),//MakeUppercaseAsync(context.Document, declaration, c), 
-                    equivalenceKey: title),
-                diagnostic);
-        }
+			// Register a code action that will invoke the fix.
+			context.RegisterCodeFix(
+				CodeAction.Create(
+					title: title,
+					createChangedDocument: c => RemoveLogsAsync(context.Document, expressionStatement, c),
+					equivalenceKey: title),
+				diagnostic);
+		}
 
-        private async Task<Document> RemoveLogsAsync(Document document, SyntaxNode expression, CancellationToken c)
-        {
-            var root = await document.GetSyntaxRootAsync();
-            var newRoot = root.RemoveNode(expression, SyntaxRemoveOptions.KeepNoTrivia);
-            var doc = document.WithSyntaxRoot(newRoot);
-            return doc;
-        }
-    }
+		private async Task<Document> RemoveLogsAsync(Document document, SyntaxNode expression, CancellationToken c) {
+			var root = await document.GetSyntaxRootAsync();
+			var newRoot = root.RemoveNode(expression, SyntaxRemoveOptions.KeepNoTrivia);
+			var doc = document.WithSyntaxRoot(newRoot);
+			return doc;
+		}
+	}
 }
